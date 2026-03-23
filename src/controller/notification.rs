5a09c004c2a2449e018281b0c3373969e0f859c9
aldofahrezy <aldofhrzy@gmail.com>
2026-03-23 23:22:57 +0700

    Implement subscribe function in Notification controller.

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -1,40 +1,10 @@
-use rocket::{get, post, serde::json::Json};
-use crate::service::notification::NotificationService;
-use crate::model::subscriber::SubscriberRequest;
+use rocket::serde::json::Json;
+use bambangshop::{Result, compose_error_response};
 use crate::model::notification::Notification;
-use bambangshop_receiver::APP_CONFIG;
+use crate::service::notification::NotificationService;
 
 #[post("/subscribe/<product_type>")]
-pub fn subscribe(product_type: &str) -> Json<SubscriberRequest> {
+pub fn subscribe(product_type: &str) -> Json<&str> {
     NotificationService::subscribe(product_type);
-    let my_url = format!("{}/receive", APP_CONFIG.get_instance_root_url());
-    Json(SubscriberRequest {
-        name: APP_CONFIG.get_instance_name().clone(),
-        url: my_url,
-    })
-}
-
-#[post("/unsubscribe/<product_type>")]
-pub fn unsubscribe(product_type: &str) -> Json<SubscriberRequest> {
-    NotificationService::unsubscribe(product_type);
-    let my_url = format!("{}/receive", APP_CONFIG.get_instance_root_url());
-    Json(SubscriberRequest {
-        name: APP_CONFIG.get_instance_name().clone(),
-        url: my_url,
-    })
-}
-
-#[post("/receive", format = "json", data = "<notification>")]
-pub fn receive(notification: Json<Notification>) -> Json<Notification> {
-    let result = NotificationService::receive_notification(notification.into_inner());
-    match result {
-        Ok(n) => Json(n),
-        Err(e) => panic!("{}", e)
-    }
-}
-
-#[get("/list")]
-pub fn list() -> Json<Vec<String>> {
-    let messages = NotificationService::list_messages();
-    Json(messages)
+    Json("Subscribed")
 }
