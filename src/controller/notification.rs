760a5dfa61b304de79262255ab03b01b0269880f
aldofahrezy <aldofhrzy@gmail.com>
2026-03-23 23:22:57 +0700

    Implement receive function in Notification controller.

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -14,3 +14,15 @@ pub fn unsubscribe(product_type: &str) -> Json<&str> {
     NotificationService::unsubscribe(product_type);
     Json("Unsubscribed")
 }
+
+#[post("/receive", data = "<payload>")]
+pub fn receive(payload: Json<Notification>) -> Result<Notification> {
+    let result = NotificationService::receive_notification(payload.into_inner());
+    match result {
+        Ok(notification) => Ok(Json(notification)),
+        Err(e) => Err(compose_error_response(
+            rocket::http::Status::BadRequest,
+            String::from(e)
+        )),
+    }
+}
